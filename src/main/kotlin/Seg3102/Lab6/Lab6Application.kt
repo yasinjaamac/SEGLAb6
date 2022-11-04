db242e0a7f37452f8dead8cf459434e3bcc1ef6d
yusufj <yusufjaamac@cmail.carleton.ca>
2022-11-04 16:54:44 -0400

    add comment

---
+++ src/main/kotlin/Seg3102/Lab6/Lab6Application.kt
@@ -0,0 +1,12 @@
+package Seg3102.Lab6
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Lab6Application
+
+fun main(args: Array<String>) {
+	runApplication<Lab6Application>(*args)
+}
+//Testing
