3879f0f0890070a612ec0ceb8a1f2417bf7b90a7
yusufj <yusufjaamac@cmail.carleton.ca>
2022-11-04 21:29:39 -0400

    update

--- src/main/kotlin/Seg3102/Lab6/controllerExceptionHandler.kt
+++ src/main/kotlin/Seg3102/Lab6/controllerExceptionHandler.kt
@@ -1,4 +1,17 @@
 package Seg3102.Lab6
 
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
 class controllerExceptionHandler {
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun handleException(ex: Exception): ResponseEntity<String> {
+        return ResponseEntity.badRequest().body("Unable to process request")
+
+    }
 }
